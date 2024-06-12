39e3b835eebc15f275c1e1aea25fa05977e03a88
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-06-12 06:19:19 -0400

    Create app

--- mysite/streamers/apps.py
+++ mysite/streamers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StreamerConfig(AppConfig):
+class StreamersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'streamer'
+    name = 'streamers'
