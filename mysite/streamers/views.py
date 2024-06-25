79e3126e4e4a1c5cb9cea27b369fde597d736e4d
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-06-24 22:56:04 -0400

    Update views.py

--- mysite/streamers/views.py
+++ mysite/streamers/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Streamer
+
+def index(request):
+    latest_question_list = Streamer.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "streamers/index.html", context)
+
+def detail(request):
+    latest_question_list = Streamer.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "streamers/detail.html", context)
+
+def about(request):
+    latest_question_list = Streamer.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "streamers/about.html", context)
