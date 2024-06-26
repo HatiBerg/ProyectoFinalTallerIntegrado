ff9b8f15eeb4e5f7e859563a81dc2a802b90cf7e
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-06-26 19:22:08 -0400

    Create css

--- mysite/streamers/urls.py
+++ mysite/streamers/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "streamers"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("streamer/", views.streamers_list, name="streamers_list"),
     path("streamer/<int:streamer_id>/", views.detail, name="detail"),
     path("about/", views.about, name="about"),
 ]
